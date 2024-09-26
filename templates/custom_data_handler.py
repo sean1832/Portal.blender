18596052fad826c5d67845c7100f0e002d7a75c7
Zeke Zhang <sean1832725142@gmail.com>
2024-09-26 10:15:59 +1000

    refactor: move `handle` method to the bottom for easier access

--- templates/custom_data_handler.py
+++ templates/custom_data_handler.py
@@ -7,12 +7,12 @@ class CustomHandler:
         self.channel_name = None
         self.channel_uuid = None
 
-    def handle(self):
-        """This is where you implement the logic to handle the custom data."""
-        print(f"Received custom data: {self.data} \nfrom channel: {self.channel_uuid}")
-
     def update(self, payload, channel_name, channel_uuid):
         """(Do not change) Update the data and channel UUID."""
         self.data = payload
         self.channel_name = channel_name
         self.channel_uuid = channel_uuid
+
+    def handle(self):
+        """This is where you implement the logic to handle the custom data."""
+        print(f"Received custom data: {self.data} \nfrom channel: {self.channel_uuid}")
